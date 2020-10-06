451d23441f164c9dddd5f35cfd11ee65d96548a8
Alex Rosen <64982042+alexrosen45@users.noreply.github.com>
2020-10-06 19:20:51 -0400

    Add files via upload

---
+++ beginner_neural_net.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+np.random.seed(0)
+
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
+
+class layer_dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = layer_dense(len(X[0]), 5)
+layer2 = layer_dense(5, 2)
+
+layer1.forward(X)
+print(layer1.output)
